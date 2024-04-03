f61e385688004c2a4fbd96a0e8be0b31aeca87c2
N0V8rs <144738466+N0V8rs@users.noreply.github.com>
2024-04-03 20:51:11 -0300

    Added the door asset
    Needs audio when the door opens and closes

--- Assets/Door/AutoDoor.cs
+++ Assets/Door/AutoDoor.cs
@@ -6,26 +6,50 @@ using UnityEngine;
 public class AutoDoor : MonoBehaviour
 {
     public GameObject Door;
+    public float speed = 1.0f; // Speed of the door's movement
+    public float targetHeight = 1.0f; // Target height for the door
+    private Vector3 initialPosition;
+    private Vector3 targetPosition;
+    private bool doorMovingUp = false;
+    private bool doorMovingDown = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialPosition = Door.transform.position;
+        targetPosition = new Vector3(Door.transform.position.x, Door.transform.position.y + targetHeight, Door.transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (doorMovingUp)
+        {
+            Door.transform.position = Vector3.Lerp(Door.transform.position, targetPosition, Time.deltaTime * speed);
+            if (Door.transform.position.y >= targetPosition.y)
+            {
+                doorMovingUp = false;
+            }
+        }
+        else if (doorMovingDown)
+        {
+            Door.transform.position = Vector3.Lerp(Door.transform.position, initialPosition, Time.deltaTime * speed);
+            if (Door.transform.position.y <= initialPosition.y)
+            {
+                doorMovingDown = false;
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Door.SetActive(false);
+        doorMovingUp = true;
+        doorMovingDown = false;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        Door.SetActive(true);
+        doorMovingDown = true;
+        doorMovingUp = false;
     }
 }
